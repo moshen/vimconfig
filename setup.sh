cf77474b8c92597c0959e66dea4496fa42b0ee49
Colin <moshen.colin@gmail.com>
2012-07-29 13:46:07 -0500

    Make the setup.sh script more robust
    Added error handling for vundle install failure
    Added handling of existing symlinks/files in home folder

--- setup.sh
+++ setup.sh
@@ -2,13 +2,35 @@
 
 cd $HOME/.vim
 
+timestamp=$(date "+%Y%m%d_%H%M%S")
+
 # Grab Vundle
 git clone git@github.com:gmarik/vundle.git bundle/vundle
 
+if [ $? -ne 0 ]; then
+  echo "Git clone failed, bailing out..."
+  exit 1;
+fi
+
 # Link up!
 cd $HOME
-ln -s .vim/vimrc .vimrc
-ln -s .vim/gvimrc .gvimrc
+
+for f in vimrc gvimrc; do
+  if [ -f ".$f" ]; then
+    if [ "$(readlink -n .$f)" == ".vim/$f" ]; then
+      echo "$HOME/.$f already links to the right file"
+    else
+      echo "$HOME/.$f exists, moving to .$f.$timestamp"
+      mv .$f .$f.$timestamp
+      ln -s .vim/$f .$f
+    fi
+  else
+    ln -s .vim/$f .$f
+  fi
+done
 
 # Install Bundles
 vim +BundleInstall +qall
+
+echo "Done!  Vim is fully configured."
+
